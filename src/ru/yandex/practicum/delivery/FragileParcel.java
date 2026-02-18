93c807df7461e0b65cf6fd75e8e73d881ac9bf50
Gurov Ilya <gyrov234@gmail.com>
2026-02-18 12:25:28 +0300

    Дополнил метод packageItem. Добавил поле базовой стоимости.

--- src/ru/yandex/practicum/delivery/FragileParcel.java
+++ src/ru/yandex/practicum/delivery/FragileParcel.java
@@ -1,26 +1,21 @@
 package ru.yandex.practicum.delivery;
 
 public class FragileParcel extends Parcel implements Trackable { //хрупкая
-    private double overPrice = 2;
 
     public FragileParcel(String description, double weight, String deliveryAddress, int sendDay) {
         super(description, weight, deliveryAddress, sendDay);
-    }
-
-    @Override
-    public void reportStatus(String newLocation) {
-        System.out.println("Хрупкая посылка <<" + this + ">> изменила местоположение на " + newLocation);
-    }
-
-    @Override
-    double baseCost() {
-        return BASE_COST + overPrice;
+        this.baseCost = 4;
     }
 
     @Override
     public <T> void packageItem(T parcel) {
         System.out.println("Посылка " + parcel + " обёрнута в защитную плёнку");
+        super.packageItem(parcel);
+    }
 
+    @Override
+    public void reportStatus(String newLocation) {
+        System.out.println("Хрупкая посылка <<" + this + ">> изменила местоположение на " + newLocation);
     }
 
     @Override
